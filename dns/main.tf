840a0c78750247b238d79826ed26e84515ee6e0b
Christian Tschenett <christian.tschenett@nhsx.nhs.uk>
2021-11-03 22:17:35 +0100

    sample: use existing HZ

--- dns/main.tf
+++ dns/main.tf
@@ -1,3 +1,7 @@
 resource "aws_route53_zone" "this" {
   name = var.domain_name
 }
+
+#data "aws_route53_zone" "existing" {
+#  name = var.domain_name
+#}
--- dns/outputs.tf
+++ dns/outputs.tf
@@ -1,11 +1,19 @@
-output "name_servers" {
-  value = aws_route53_zone.this.name_servers
-}
-
 output "domain_name" {
   value = var.domain_name
+}
+
+output "name_servers" {
+  value = aws_route53_zone.this.name_servers
 }
 
 output "zone_id" {
   value = aws_route53_zone.this.id
 }
+
+#output "name_servers" {
+#  value = data.aws_route53_zone.existing.name_servers
+#}
+
+#output "zone_id" {
+#  value = data.aws_route53_zone.existing.id
+#}
